1824b96e704eb5d82cd14dafb51746fea5ae6d61
Ravindu Kaveesha <ravindu.kaveesha2001@gmail.com>
2026-06-16 20:18:39 +0530

    remove usless components

--- frontend/src/pages/Home/HomePage.tsx
+++ frontend/src/pages/Home/HomePage.tsx
@@ -30,6 +30,7 @@ export default function HomePage({ company, contactStatus, quoteStatus, onContac
       <HomeProjectsSection />
       <HomeClientsSection />
       <HomeCtaSection/>
+      
     </main>
   );
 }
